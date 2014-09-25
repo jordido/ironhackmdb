48730034468bd0f9047a272e1e44926319b220c5
jordido <jordi@zona-e.com>
2014-09-25 19:30:53 +0200

    works the first add by erp

--- ironhackmdb.rb
+++ ironhackmdb.rb
@@ -5,7 +5,7 @@ require 'sinatra/reloader'
 require 'date'
 require "pry"
 require 'imdb'
-require_relative 'List'
+#require_relative 'List'
 set :port, 3000
 set :bind, '0.0.0.0'
 
@@ -14,22 +14,12 @@ ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: 'ironhackmdb.sqlite'
 )
-#config.i18n.enforce_available_locales = true
 class TVShow < ActiveRecord::Base
-	
-	#The name field should be present, and unique across all our TV shows.
- 	#The our_rating field should be present and between 0 and 10 (both included)
-	#The our_comments field should be present, with more than 100 characters but less than 10000
-	
+		
   validates_presence_of :name, :own_rating, :own_comments   
   validates_uniqueness_of :name
 	validates :own_rating, numericality: true, inclusion: {in: (0..10)}
 	validates :own_comments, length: {in: 10..10000}	
-#	attr_accessor :imdb_rating
-
-	# def initialize
-	# 	@imdb_rating = get_imdb_rating ('Friends')
-	# end
 
 	private
 
@@ -39,23 +29,22 @@ class TVShow < ActiveRecord::Base
 
 end
 
-list_of_shows ||= TVShow.all
 
 get '/' do
-	@list = list_of_shows
-	erb :index  # goes to views folder and looks for the file index.erb (html)
+	@list = TVShow.all
+	erb :index  
 end
 
 post '/' do
-
 	if params[:action] == 'add'
-		if list_of_songs.songs.size < 5
-			list_of_songs.add_song(params[:artist], params[:song])
-		  @list = list_of_songs.songs
-			erb :index
-		else
-			redirect to('/enough')
-		end
+		my_show = params[:show]
+		my_comments = params[:comments]
+		my_rating = params[:rating]
+		myshow=TVShow.create(name: my_show, own_comments: my_comments, own_rating: my_rating.to_i)
+		myshow.save
+		@list = TVShow.all
+		erb :index
+	else
 	end	
 end
 
